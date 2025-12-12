5e9624d0ff1b09c1f389777bbde77d57b31e9ec3
Koodal Kweon <kkweon@f1r4s14.codam.nl>
2025-12-12 15:16:30 +0100

    norminette check

--- get_next_line.c
+++ get_next_line.c
@@ -6,64 +6,62 @@
 /*   By: kkweon <kkweon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 15:52:43 by kkweon            #+#    #+#             */
-/*   Updated: 2025/12/12 14:03:01 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/12/12 15:14:31 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *left_str(char *left)
+char	*left_str(char *left)
 {
-	int i;
-	int j;
-	char *tmp;
-	char *res;
+	int		i;
+	char	*tmp;
+	char	*res;
 
 	i = 0;
-	j = 0;
 	tmp = left;
 	while (tmp[i])
 	{
-		if (tmp[i] == '\\' && tmp[i + 1] == 'n')
+		if (tmp[i] == '\n')
 		{
-			res = ft_substr(tmp, j, (size_t)i);
-			return (tmp);
+			res = ft_substr(tmp, 0, (size_t)i);
+			return (res);
 		}
-		else
-			i++;
+		i++;
 	}
-	return (tmp);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
-	int line_len;
-	char *buff;
-	char *str_dup;
-	char *tmp;
+	int		line_len;
+	char	*buff;
+	char	*tmp;
 
-	buff = malloc(BUFFER_SIZE + 1 * sizeof(char));
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (buff == NULL || BUFFER_SIZE <= 0)
 		return (NULL);
-	tmp = left_str(buff);
-	line_len = read(fd, tmp, BUFFER_SIZE);
+	line_len = read(fd, buff, BUFFER_SIZE);
 	if (line_len <= 0)
-	{
-		free(buff);
-		return (NULL);
-	}
-
+		return (free(buff), NULL);
+	buff[line_len] = '\0';
+	tmp = left_str(buff);
 	return (tmp);
 }
 
 int main (void)
 {
 	int fd;
+	int count;
 	char *res;
+
 	fd = open("test.txt", O_RDONLY);
+	printf("[fd No.]: %d\n", fd);
 	res= get_next_line(fd);
-	
-	printf("%s\n", res);
+	count = 1;
+	printf("[Final result %d]: %s\n", count++, res);
+	printf("[Final result %d]: %s\n", count++, res);
+	printf("[Final result %d]: %s\n", count++, res);
 	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 /*   By: kkweon <kkweon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 16:36:33 by kkweon            #+#    #+#             */
-/*   Updated: 2025/12/12 13:57:21 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/12/12 15:07:11 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdarg.h>
 # include <stdio.h>
@@ -19,7 +19,7 @@
 # include <fcntl.h>
 # include "get_next_line.h"
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
+#  define BUFFER_SIZE 10
 # endif
 
 char	*get_next_line(int fd);
